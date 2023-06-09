640077483e2952d10e4cb6c0c1ce4009241e6dd4
Anurag Rawat <75340540+garuna55@users.noreply.github.com>
2023-06-10 01:52:31 +0530

    Create second_challenge.sol

---
+++ second_challenge.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.9;
+
+contract assessment_2 {
+    
+     uint public constant oneWei = 1 wei;
+     uint public constant oneeEther = 1 ether;
+     uint public constant oneeGwei = 1 gwei;
+    
+}
