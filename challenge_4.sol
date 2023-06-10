3d2579e0d6e1c40ad4fa6cec0ca5bebe83ceb437
Anurag Rawat <75340540+garuna55@users.noreply.github.com>
2023-06-10 18:34:23 +0530

    Update challenge_4.sol

--- challenge_4.sol
+++ challenge_4.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.5;
 contract challenge4{
+
+// simple loop function will work . 
+
     function loop() external pure returns(uint)  {
     uint x = 0;
     while (x<1000){
@@ -10,7 +13,7 @@ contract challenge4{
 
     return x;
     }
-
+// but the complex loop is not deployed as it ran out of gas limit .
     function complexloop() external pure returns(uint) {
         uint sum = 0 ;
 
